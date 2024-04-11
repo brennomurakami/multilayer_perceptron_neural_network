6281a186570cdc7274664a6f94044ae9476534bf
Brenno Murakami <brenno.murakami@gmail.com>
2024-04-11 07:46:48 -0300

    'good' and 'bad' values on the data changed to 1 and 0 respectively when the data_read function runs

--- multiperceptron.py
+++ multiperceptron.py
@@ -16,16 +16,15 @@ def read_excel(file_path, percent_of_training):
 
     data = []
 
-    # Capturing dat
+    # Capturing all data
     for row_index, row in enumerate(ws.iter_rows(values_only=True), start=1):
         # ignoring header
-        # Size / Weight / Sweetness / Crunchiness / Juiciness / Ripeness / Acidity
         if row_index == 1:
             continue
         # Adding line data
-        data.append(row)
+        data.append(list(row))
 
-    # calculing training
+    # calculating training
     total_rows = len(data)
     num_training_rows = int(percent_of_training * total_rows)
 
@@ -35,6 +34,19 @@ def read_excel(file_path, percent_of_training):
     # Separar dados de treinamento e teste
     training_data = data[:num_training_rows]
     testing_data = data[num_training_rows:]
+
+    # Modificar os dados na última coluna
+    for i in range(len(training_data)):
+        if training_data[i][-1] == "good":
+            training_data[i][-1] = 1
+        else:
+            training_data[i][-1] = 0
+
+    for i in range(len(testing_data)):
+        if testing_data[i][-1] == "good":
+            testing_data[i][-1] = 1
+        else:
+            testing_data[i][-1] = 0
 
     return training_data, testing_data
 
@@ -54,3 +66,4 @@ print("Rows for Training Data: ", len(train_data))
 print("Rows for Test Data: ", len(test_data))
 
 print(train_data)
+
